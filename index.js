963301865287cd10765bb8363521fb66952ca041
unknown <mydulcse62.niter@gmail.com>
2024-06-11 17:01:04 +0600

    create route and successfully update user informations

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 });
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xm07hcd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -39,6 +39,7 @@ async function run() {
     const upazillasCollection = database.collection("Upazillas");
     const AllTestCollection = database.collection("AllTest");
     const AlluserCollection = database.collection("Allusers");
+    const AllBookedCollection = database.collection("allBooked");
 
     // Send a ping to confirm a successful connection
     app.get('/district',async(req, res) =>{
@@ -49,21 +50,122 @@ async function run() {
      const result = await upazillasCollection.find().toArray();
      res.send(result);
     })
-    app.get('/alltests',async(req, res) =>{
-     const result = await AllTestCollection.find().toArray();
-     res.send(result);
-    })
+    app.get('/alltests', async (req, res) => {
+      try {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        const formattedToday = today.toISOString().split('T')[0];
+      
+        const result = await AllTestCollection.find({ date: { $gte: formattedToday } }).sort({ date: 1 }).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while processing your request.');
+      }
+    });
+    
+    app.get('/alltests/date/:date', async (req, res) => {
+      try {
+        const { date } = req.params;
+        console.log(`Searching for records with date: ${date}`);
+    
+        const result = await AllTestCollection.find({ date }).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while processing your request.');
+      }
+    });
+    
     app.get('/allusers',async(req, res) =>{
         const result = await AlluserCollection.find().toArray();
         res.send(result);
     });
+    app.get('/allusers/email/:email',async(req, res) =>{
+     const email = req.params.email;
+      const query = {emailAdress:email}  
+      const result = await AlluserCollection.findOne(query);
+        res.send(result);
+    });
+    app.get('/Alltests/tests/test/:id',async(req, res) =>{
+     const id = req.params.id;
+      const query = {_id: new ObjectId(id)}   
+      const result = await AllTestCollection.findOne(query);
+        res.send(result);
+    });
     
+    
+    app.post('/alltest',async(req, res) =>{
+        const result = await AllTestCollection.insertOne(req.body);
+        res.send(result);
+    });
+    app.post("/alltest/Booking",async(req, res) =>{
+        const result = await AllBookedCollection.insertOne(req.body);
+        res.send(result);
+    });
+    app.get("/alltest/Booking",async(req, res) =>{
+        const result = await AllBookedCollection.find().toArray();
+        res.send(result);
+    });
+    app.get("/alltest/Booking/:email",async(req, res) =>{
+    const email = req.params.email;
+    const query = {userEmail:email}
+        const result = await AllBookedCollection.find(query).toArray();
+        res.send(result);
+    });
+    app.delete("/alltest/Booking/Delete/:id",async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};   
+      const result = await AllBookedCollection.deleteOne(query);
+      res.send(result);
+    })
     
     app.post('/allusers',async(req, res) =>{
         const result = await AlluserCollection.insertOne(req.body);
         res.send(result);
     });
+    // ---------------------------------------------------
+    app.patch('/allusers/Updates/update/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = {emailAdress:email}  
+
+      const { userName, district, upazilla, imageUrl, bloodGrupe } = req.body;
     
+      console.log('Received email:', email);
+    
+      const updateInformations = {
+        $set: {
+          name: userName,
+          districs: district,
+          upazella: upazilla,
+          image: imageUrl,
+          bloodGrupe: bloodGrupe,
+        },
+      };
+    
+   
+        const result = await AlluserCollection.updateOne(query, updateInformations);
+        console.log('Update result:', result);
+      console.log(updateInformations);
+    });
+    
+    
+
+    
+    app.patch("/alltest/slot/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateSlot = req.body.slot; 
+        const updatedDoc = {
+          $set: {
+              slot: updateSlot
+          }
+      };
+  
+      const result = await AllTestCollection.updateOne(query, updatedDoc);
+      res.send(result);
+  });
+  
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
